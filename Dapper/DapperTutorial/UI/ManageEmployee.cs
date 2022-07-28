09d060b61f1676fc81e2e53806421c60d16b507b
Jacob Swineford <43424810+JacobSwineford@users.noreply.github.com>
2022-07-28 10:52:20 -0400

    Add files via upload

---
+++ Dapper/DapperTutorial/UI/ManageEmployee.cs
@@ -0,0 +1,48 @@
+﻿using DapperTutorial.Core.Entities;
+using DapperTutorial.Infrastructure.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DapperTutorial.UI
+{
+    public class ManageEmployee
+    {
+
+        EmployeeRepository er;
+
+        ManageEmployee()
+        {
+            er = new EmployeeRepository();
+        }
+
+        void AddEmployee(Employee e)
+        {
+            er.Insert(e);
+        }
+
+        void updateEmployee(Employee e)
+        {
+            er.Update(e);
+        }
+
+        void RemoveEmployee(Employee e)
+        {
+            er.DeleteById(e.Id);
+        }
+
+        Employee GetEmployee(int id)
+        {
+            return er.GetById(id); 
+        }
+
+        IEnumerable<Employee> GetAllEmployees()
+        {
+            return er.GetAll();
+        }
+
+
+    }
+}
