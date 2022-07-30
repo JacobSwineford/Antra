823b4643d017ac29ae16f528d60e2386f2d0f156
Jacob Swineford <43424810+JacobSwineford@users.noreply.github.com>
2022-07-30 19:39:25 -0400

    Add files via upload

---
+++ MVC/AsyncConsole2/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+namespace na
+{
+    public class foo
+	{
+        public static void Main()
+		{
+           
+        }
+
+        public async Task SimpleWriteAsync()
+        {
+            string filePath = "simple.txt";
+            string text = $"Hello World";
+
+            await File.WriteAllTextAsync(filePath, text);
+        }
+    }
+
+    
+}
