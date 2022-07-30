61b6363d21b9883762064568227a99d9a9ff3dc7
Jacob Swineford <43424810+JacobSwineford@users.noreply.github.com>
2022-07-30 19:47:14 -0400

    Add files via upload

---
+++ MVC/MVC Project/Controllers/CastController.cs
@@ -0,0 +1,37 @@
+﻿using Core.Contracts.Service;
+using Core.Entities;
+using Microsoft.AspNetCore.Mvc;
+using MVC_Project.Models;
+
+namespace MVC_Project.Controllers
+{
+    public class CastController : Controller
+    {
+        ICastServiceAsync service;
+        public CastController(ICastServiceAsync _service)
+        {
+            service = _service;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Details()
+        {
+            Task<IEnumerable<Cast>> cast = service.GetCastDetails();
+            IEnumerable<Cast> result = cast.Result;
+            List<CastData> model = new List<CastData>();
+            foreach (var c in result)
+            {
+                CastData ca = new CastData();
+                ca.Name = c.Name;
+                ca.Id = c.Id;
+                ca.Gender = c.Gender;
+                model.Add(ca);
+            }
+            return View(model);
+        }
+    }
+}
---
+++ MVC/MVC Project/Controllers/MovieController.cs
@@ -0,0 +1,62 @@
+﻿using Core.Contracts.Service;
+using Core.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_Project.Controllers
+{
+    public class MovieController : Controller
+    {
+
+        IMovieServiceAsync service;
+
+        public MovieController(IMovieServiceAsync _service)
+        {
+            service = _service;
+        }
+
+        public IActionResult Index(string name)
+        {
+            Console.WriteLine("name index: " + name); // test to see if I can detect name, can be used to navigate through details
+            return View();
+        }
+
+        public IActionResult List(string name)
+		{
+            //int pages = 4;
+            //int perPage = 3;
+            Task<IEnumerable<Movie>> results = service.GetAllMoviesAsync();
+            List<Movie> dataSet = results.Result.ToList();
+            //List<List<Movie>> paginatedMovies = PaginatedResults(dataSet, perPage);
+            Console.WriteLine("name list: " + name);
+            return View(dataSet);
+        }
+
+        public IActionResult Details(int movieId)
+		{
+            Task<Movie> result = service.GetByIdAsync(movieId);
+            return View(result.Result);
+		}
+
+        private List<List<Movie>> PaginatedResults(List<Movie> dataSet, int perPage)
+		{
+            List<List<Movie>> paginated = new List<List<Movie>>();
+            int counter = 0;
+            List<Movie> sub = null;
+            foreach (var m in dataSet)
+			{
+                if (counter % perPage == 0)
+				{
+                    if (sub != null)
+                        paginated.Add(sub);
+                    sub = new List<Movie>();
+				}
+                    
+                sub.Add(m);
+			}
+            if (sub.Count != 0)
+                paginated.Add(sub);
+
+            return paginated;
+		}
+    }
+}
